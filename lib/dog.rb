1f085031205f77fb914e4c25413bd0c49cf35561
Marcus Magnusson <marcusmagnusson@outlook.com>
2016-12-20 02:57:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+class Dog
+  def name= (name)
+    @name = name
+  end
+
+  # has a name     
+  def name
+    @name
+  end
+
+  # is able to bark
+  def bark
+    puts "woof!"
+  end
+end
+
+# Should be able to:
+# fido = Dog.new
+# fido.name = "Fido"
+
+# fido.name
+# => "Fido"
+
+# fido.bark
+# => woof!
